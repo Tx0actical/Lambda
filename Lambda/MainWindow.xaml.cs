2ecbf661279fc7bdc20622d01d3c69cd61658ba1
Amit Panghal <88306361+Tx0actical@users.noreply.github.com>
2023-02-26 09:47:18 +0530

    added navigation pane but rough edges remain

--- Lambda/MainWindow.xaml.cs
+++ Lambda/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using static System.Net.WebRequestMethods;
 
 
 namespace Lambda {
@@ -27,13 +29,26 @@ namespace Lambda {
             this.InitializeComponent ();
         }
         private void advButton_Click (object sender, RoutedEventArgs e) {
-            advblock.Text = "Sample Sent! Awaiting response..";
+            advblock.Text = "Sample Sent. Awaiting response...";
             AdvancedButton.Visibility = Visibility.Collapsed;
             _clicks++;
             if (_clicks == 1)
                 advprogressbar.Visibility = Visibility.Visible;
             //if (_response)
             //    advprogressbar.IsIndeterminate = false;
+        }
+        private void ContentFrame_NavigationFailed (object sender, NavigationFailedEventArgs e) {
+            throw new Exception ("Failed to load Page " + e.SourcePageType.FullName);
+        }
+        
+        private void NavView_ItemInvoked (NavigationView sender, NavigationViewItemInvokedEventArgs e) {
+            NavView.MenuItems.Add (new NavigationViewItem {
+                Content = "Advanced Scanning",
+                Tag = "FileScanningPage"
+            });
+        }
+        private void NavView_Loaded (object sender, RoutedEventArgs e) {
+
         }
     }
 }
