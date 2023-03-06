09571b50528e6ee8c6020f0bd7f0acc2b8950461
Amit Panghal <88306361+Tx0actical@users.noreply.github.com>
2023-03-07 00:49:38 +0530

    added file picker, modified layout a bit

--- Lambda/MainWindow.xaml.cs
+++ Lambda/MainWindow.xaml.cs
@@ -1,29 +1,17 @@
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
 using static System.Net.WebRequestMethods;
 
 
 namespace Lambda {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
 
     public sealed partial class MainWindow : Window {
         private static int _clicks = 0;
-        public bool _response = false;
+        public int _success_response_status_code = 200;
 
         public MainWindow () {
             this.InitializeComponent ();
@@ -37,18 +25,50 @@ namespace Lambda {
                 advblock.Text = "Error Receiving Response";
                 advprogressbar.Visibility = Visibility.Visible;
             }
+            AdvancedButton = (Button) sender;
+
         }
 
         private void ContentFrame_NavigationFailed (object sender, NavigationFailedEventArgs e) {
             throw new Exception ("Failed to load Page " + e.SourcePageType.FullName);
         }
-        
+
         private void NavView_ItemInvoked (NavigationView sender, NavigationViewItemInvokedEventArgs e) {
-            
+
         }
 
         private void NavView_Loaded (object sender, RoutedEventArgs e) {
 
         }
+
+        private async void PickAFileButton_Click (object sender, RoutedEventArgs e) {
+            // Clear previous returned file name, if it exists, between iterations of this scenario
+            PickAFileOutputTextBlock.Text = "";
+
+            // Create a file picker
+            var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+
+            // Retrieve the window handle (HWND) of the current WinUI 3 window.
+            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            WindowId myWndId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+
+
+            // Initialize the file picker with the window handle (HWND).
+            WinRT.Interop.InitializeWithWindow.Initialize (openPicker, hWnd);
+
+            // Set options for your file picker
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.FileTypeFilter.Add ("*");
+
+            // Open the picker for the user to pick a file
+            var file = await openPicker.PickSingleFileAsync();
+            if (file != null) {
+                PickAFileOutputTextBlock.Text = "Selected File : " + file.Name;
+            } else {
+                PickAFileOutputTextBlock.Text = "Operation cancelled";
+            }
+
+        }
     }
 }
+
